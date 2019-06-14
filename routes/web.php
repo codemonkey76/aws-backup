4782d15be8a8e5d62f862f7dec9082b4689b41ec
Shane Poppleton <shane@bjja.com.au>
2019-06-14 16:30:04 +1000

    Syntax error in web.php

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::post('/command', 'CommandController@execute')->name('command')->;
+Route::post('/command', 'CommandController@execute')->name('command');
