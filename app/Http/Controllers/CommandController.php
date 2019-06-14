e0cf5ebb925716c32adc23a3f398e53ed60934eb
Shane Poppleton <shane@bjja.com.au>
2019-06-14 16:40:09 +1000

    Added CSRF on Command

--- app/Http/Controllers/CommandController.php
+++ app/Http/Controllers/CommandController.php
@@ -9,6 +9,10 @@ class CommandController extends Controller
 {
     public function execute(Request $request)
     {
+        if ($request['token']!== env('SLACK_COMMAND_TOKEN')) {
+            return response(419);
+        }
+        
         Log::info($request->all());
         return "Running the command you requested";
     }
