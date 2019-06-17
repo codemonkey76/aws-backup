873676a19b8d30b632b7856fab1108bce7f34c35
Shane Poppleton <shane@alphaitcentre.com.au>
2019-06-18 08:57:38 +1000

    Widen table

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-10">
                 <form method="POST" action="{{route('tasks.update', $task)}}">
                     @csrf
                     @method('PATCH')
