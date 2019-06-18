605ccb0e71dab87582e4023773d856d48bf97be3
Shane Poppleton <shane@alphaitcentre.com.au>
2019-06-18 15:57:12 +1000

    Work on the SlashCommand

--- app/Http/Controllers/CommandController.php
+++ app/Http/Controllers/CommandController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Str;
 
 class CommandController extends Controller
 {
@@ -18,8 +18,24 @@ class CommandController extends Controller
         }
         $args = explode(' ',$request['text']);
 
+        $command_args = [];
         if (in_array($args[0], $validCommands)) {
             $message .= "Valid command received: $args[0]";
+            $options = array_slice($args, 1);
+            foreach ($options as $option) {
+                if (substr($option, 0, 2) === "--") {
+                    $has_val=strpos($option, '=');
+                    if ($has_val) {
+                        $command_args[substr($option,2,$has_val-2)]=substr($option, $has_val+1);
+                    } else {
+                        $command_args[substr($option, 2)]=true;
+                    }
+                }
+                else {
+                    $command_args[''] = $option;
+                }
+            }
+            Artisan::call($args[0], $command_args);
         }
         else {
             $message .= "Invalid command: $args[0], valid commands are " . implode($validCommands, ' ');
